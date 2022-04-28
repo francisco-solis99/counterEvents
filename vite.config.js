3e551b1ca3e0d365358f1297e1e3d0d74a4d940f
francisco-solis99 <franciscosoliscss@gmail.com>
2022-04-27 21:07:16 -0500

    [Fix] the vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
-export default defineConfig({
+const path = require('path');
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+const base = mode === 'production' ? '/' + path.basename(process.cwd()) + '/' : '/';
+
+module.exports = {
+  root: 'src',
+  base,
+  mode,
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+    assetsDir: './'
+  },
   server: {
     port: 3030
-  },
-});
+  }
+};
